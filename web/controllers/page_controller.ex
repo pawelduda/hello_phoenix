4b440b4fe161b337dffc4bc54ac262316167869c
pawelduda <pawelduda91@gmail.com>
2016-08-29 22:19:14 +0200

    Pattern match on content-type

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -8,7 +8,11 @@ defmodule HelloPhoenix.PageController do
     |> render("index.html")
   end
 
-  def show(conn, %{"id" => id}) do
+  def show(conn, %{"id" => id, "content_type" => "json"}) do
+    json(conn, %{id: id})
+  end
+
+  def show(conn, %{"id" => id, "content_type" => "plain_text"}) do
     text(conn, "Showing id: #{id}")
   end
 end
