42fb99fb6563ef725124794d26d89fb84f89ec6b
pawelduda <pawelduda91@gmail.com>
2016-08-29 22:14:54 +0200

    Plaintext rendering

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,6 +2,13 @@ defmodule HelloPhoenix.PageController do
   use HelloPhoenix.Web, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    conn
+    |> put_flash(:info, "Welcome!")
+    |> put_flash(:error, "Let's pretend we have an error.")
+    |> render("index.html")
+  end
+
+  def show(conn, %{"id" => id}) do
+    text(conn, "Showing id: #{id}")
   end
 end
--- web/router.ex
+++ web/router.ex
@@ -17,6 +17,7 @@ defmodule HelloPhoenix.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+    get "/:id", PageController, :show
     get "/hello", HelloController, :index
     get "/hello/:messenger", HelloController, :show
   end
